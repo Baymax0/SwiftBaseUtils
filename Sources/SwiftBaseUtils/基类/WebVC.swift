31fbd025dbd115e0b47ded3a6db0c707582bfd10
lzw <jzdd327@163.com>
2021-12-08 17:20:45 +0800

    1
    1

--- Sources/SwiftBaseUtils/基类/WebVC.swift
+++ Sources/SwiftBaseUtils/基类/WebVC.swift
@@ -19,7 +19,7 @@ class WebVC: BaseVC{
 
     var webView:WKWebView = WKWebView( frame: CGRect(x:0, y:0, width:KScreenWidth, height:KHeightInNav))
     
-    var showNaviBar:Bool = false
+    var showNaviBar:Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,29 +32,27 @@ class WebVC: BaseVC{
         webView.backgroundColor = .KBGGray
         self.view.addSubview(webView)
         
-        if showNaviBar {
-            let naviView = UIView(frame: CGRect(x: 0, y: 0, width: KScreenWidth, height: KNaviBarH))
-            let lab = UILabel(frame: CGRect(x: 0, y: KNaviBarH-44, width: KScreenWidth, height: 44))
-            lab.text = self.title
-            lab.textAlignment = .center
-            lab.font = UIFont.boldSystemFont(ofSize: 16)
-            naviView.addSubview(lab)
-            
-            let back = UIButton(frame: CGRect(x: 10, y: KNaviBarH-44, width: 50, height: 44))
-            let img = UIImage(named: "BMback_Icon")?.withRenderingMode(.alwaysTemplate)
-            back.setImage(img, for: .normal)
-            back.tag = 0
-            back.tintColor = .black
-            back.addTarget(self, action: #selector(WebVC.back), for: .touchUpInside)
-            
-            naviView.addSubview(back)
-            naviView.backgroundColor = .white
-            
-            webView.y = KNaviBarH
-            self.view.addSubview(naviView)
-        }else{
-            
-        }
+        let naviView = UIView(frame: CGRect(x: 0, y: 0, width: KScreenWidth, height: KNaviBarH))
+        let lab = UILabel(frame: CGRect(x: 0, y: KNaviBarH-44, width: KScreenWidth, height: 44))
+        lab.text = self.title
+        lab.textAlignment = .center
+        lab.font = UIFont.boldSystemFont(ofSize: 16)
+        naviView.addSubview(lab)
+        
+        let back = UIButton(frame: CGRect(x: 10, y: KNaviBarH-44, width: 50, height: 44))
+        let img = UIImage(named: "BMback_Icon")?.withRenderingMode(.alwaysTemplate)
+        back.setImage(img, for: .normal)
+        back.tag = 0
+        back.tintColor = .black
+        back.addTarget(self, action: #selector(WebVC.back), for: .touchUpInside)
+        
+        naviView.addSubview(back)
+        naviView.backgroundColor = .white
+        
+        webView.y = KNaviBarH
+        self.view.addSubview(naviView)
+        webView.bm.addConstraints([.margin(KNaviBarH, 0, 0, 0)])
+        
     }
 
     @objc func myBack(_ btn:UIButton){
